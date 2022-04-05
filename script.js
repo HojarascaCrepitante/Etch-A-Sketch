ba100b4d0343c24666e9a324d8920484b8cff78c
Lucas Vallejos <lucascolegioripari@gmail.com>
2022-04-05 00:43:46 -0300

    Cleaned up the function, i changed the style in css by adding a class on hover, instead of changing the style in js, I think this method is more clean

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const container = document.getElementById('container')
+const button = document.createElement('button')
+button.addEventListener('click', clearGrid)
+container.appendChild(button)
 const rowAndColumns = document.createElement('div');
 rowAndColumns.classList.add('rowAndColumns');
 container.appendChild(rowAndColumns)
@@ -6,7 +9,18 @@ container.appendChild(rowAndColumns)
         for(let i = 0; i < 256; i++){
             let divsRowAndColumns = document.createElement('div');
             divsRowAndColumns.classList.add('rowAndColumnsDivs')
+            divsRowAndColumns.addEventListener('mouseenter',paintItBlack)
             rowAndColumns.appendChild(divsRowAndColumns);
         }
     
     }makeGrid()
+
+    function paintItBlack(e){
+        e.target.classList.add('active')
+    }
+    function clearGrid(){
+        let everyDivInsideGrid = document.querySelectorAll('.active')
+        for(let n = 0; n < everyDivInsideGrid.length; n++){
+            everyDivInsideGrid.classList.remove('active')
+        }
+    }
